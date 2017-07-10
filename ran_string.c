5657ef697e61c55eb023221cfe8161b30ec17e29
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2017-07-09 21:13:40 -0300

    random string generator

---
+++ ran_string.c
@@ -0,0 +1,23 @@
+/*
+Function that generates random strings.
+
+Taken from https://codereview.stackexchange.com/a/29200
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static char *rand_string(char *str, size_t size)
+{
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (size) {
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int) (sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+    return str;
+}
+
+
