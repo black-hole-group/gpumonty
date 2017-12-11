886cbfdd9809364e1cede785e859e4789819312f
gustavo <gustavo.9891@gmail.com>
2017-12-11 16:58:34 -0200

    minor change

--- sed_maker.py
+++ sed_maker.py
@@ -2,12 +2,13 @@ import nmmn.sed
 import matplotlib.pyplot as plt
 
 s=nmmn.sed.SED()
-s.grmonty('grmonty.spec')
+#s.grmonty('grmonty.spec')
+s.grmonty('spectrum.dat')
 
 plt.plot(s.lognu, s.ll)
 plt.xlim(8,23)
 plt.ylim(20,40)
 plt.xlabel(r'$\log\;\nu\;(\mathrm{Hz})$', fontsize=16)
 plt.ylabel(r'$\nu L_{\nu}\;(\mathrm{erg\;s}^{-1})$', fontsize=16)
-plt.title(r'$N_s = 10^6$', fontsize=20)
+plt.title(r'$N_s = 10^3$', fontsize=20)
 plt.show()
