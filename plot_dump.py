cd123b604094eb06848b6763cab6ac7a0f4bb6da
gustavo <gustavo.9891@gmail.com>
2018-01-24 10:01:17 -0200

    minor modifications

---
+++ plot_dump.py
@@ -0,0 +1,95 @@
+# -*- coding: utf8 -*-
+
+import numpy as np
+import scipy.interpolate
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import nmmn.lsd
+import math
+
+
+listr   = []
+listth  = []
+listx1  = [] # ln(r)
+listx2  = []
+listrho = []
+listu   = []
+listvr  = []
+listvth = []
+listvph = []
+listBr  = []
+listBth = []
+listBph = []
+listx = []
+listy = []
+
+f = open('dump019', 'r')
+header = f.readline()
+
+for line in f:
+    col = line.split()
+    listx1.append(float(col[0]))
+    listx2.append(float(col[1]))
+    listr.append(float(col[2]))
+    listth.append(float(col[3]))
+    listrho.append(float(col[4]))
+    listu.append(float(col[5]))
+    listvr.append(float(col[6]))
+    listvth.append(float(col[7]))
+    listvph.append(float(col[8]))
+    listBr.append(float(col[9]))
+    listBth.append(float(col[10]))
+    listBph.append(float(col[11]))
+#    listx.append(float(col[2]) * np.cos(float(col[3])))
+#    listy.append(float(col[2]) * np.sin(float(col[3])))
+    listx.append(float(col[2]) * np.cos(float(col[3]) - np.pi/2.))
+    listy.append(float(col[2]) * np.sin(float(col[3]) - np.pi/2.))
+
+f.close()
+
+r   = np.array(listr)
+th  = np.array(listth)
+rho = np.array(listrho)
+vr  = np.array(listvr)
+vth = np.array(listvth)
+vph = np.array(listvph)
+Br  = np.array(listBr)
+Bth = np.array(listBth)
+Bph = np.array(listBph)
+x = np.array(listx)
+y = np.array(listy)
+
+#print('r_min  = %s') %np.amin(r)
+#print('r_max  = %s') %np.amax(r)
+#print('th_min = %s') %np.amin(th)
+#print('th_max = %s') %np.amax(th)
+
+def prepare_regrid(x, y):
+    xsize = 256
+    ysize = 256 
+    fact = 1 # "aumento de resolução" para o espaçamento grid novo
+    newdx = fact*xsize
+    newdy = fact*ysize
+    xnew = np.linspace(round(np.amin(x)), round(np.amax(x)), newdx)
+    ynew = np.linspace(round(np.amin(y)), round(np.amax(y)), newdy)   
+    return xnew, ynew
+
+def newgrid(x, y, z, xnew, ynew):
+    znew = scipy.interpolate.griddata((x, y), z, (xnew[None,:], ynew[:,None]), method='cubic')
+    return znew
+
+# vv = vr*vr + vth*vth + vph*vph
+# vv = vr*vr + vth*vth + vph*vph # do same with magnetic field
+
+xnew, ynew = prepare_regrid(x, y)
+newrho = newgrid(x, y, rho, xnew, ynew)
+
+plt.imshow(np.log10(newrho), extent=(np.amin(x), np.amax(x), np.amin(y), np.amax(y)), cmap=cm.hot)
+# in this line, we'll add the B streamlines
+plt.xlabel('x $[r_g]$', fontsize=16)
+plt.ylabel('y $[r_g]$', fontsize=16)
+#plt.xlim(0, 10)
+#plt.ylim(-5, 5)
+plt.colorbar()
+plt.title('Density at $t=950\;GM/c^3$', fontsize=20)
+plt.show()
---
+++ sed_maker.py
@@ -0,0 +1,13 @@
+import nmmn.sed
+import matplotlib.pyplot as plt
+
+s=nmmn.sed.SED()
+s.grmonty('grmonty.spec')
+
+plt.plot(s.lognu, s.ll)
+plt.xlim(8,23)
+plt.ylim(20,40)
+plt.xlabel(r'$\nu$', fontsize=16)
+plt.ylabel(r'$\nu L_{\nu}$', fontsize=16)
+plt.title(r'$N_s = 10^6$', fontsize=20)
+plt.show()
