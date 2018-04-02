7fed9204fc8d6ff59a6609a0f63d639012cb628f
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-04-02 19:17:49 -0300

    removed blurb from source

--- grmonty.c
+++ grmonty.c
@@ -1,49 +1,4 @@
-
-/***********************************************************************************
-    Copyright 2013 Joshua C. Dolence, Charles F. Gammie, Monika Mo\'scibrodzka,
-                   and Po Kin Leung
-
-                        GRMONTY  version 1.0   (released February 1, 2013)
-
-    This file is part of GRMONTY.  GRMONTY v1.0 is a program that calculates the
-    emergent spectrum from a model using a Monte Carlo technique.
-
-    This version of GRMONTY is configured to use input files from the HARM code
-    available on the same site.   It assumes that the source is a plasma near a
-    black hole described by Kerr-Schild coordinates that radiates via thermal 
-    synchrotron and inverse compton scattering.
-    
-    You are morally obligated to cite the following paper in any
-    scientific literature that results from use of any part of GRMONTY:
-
-    Dolence, J.C., Gammie, C.F., Mo\'scibrodzka, M., \& Leung, P.-K. 2009,
-        Astrophysical Journal Supplement, 184, 387
-
-    Further, we strongly encourage you to obtain the latest version of 
-    GRMONTY directly from our distribution website:
-    http://rainman.astro.illinois.edu/codelib/
-
-    GRMONTY is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    GRMONTY is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with GRMONTY; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-***********************************************************************************/
-
-
 /* 
-
-   grmonty Nph
-
    Using monte carlo method, estimate spectrum of an appropriately
    scaled axisymmetric GRMHD simulation as a function of 
    latitudinal viewing angle.
@@ -61,23 +16,6 @@
 
    The electrons are assumed to have a thermal distribution 
    function, and to be at the same temperature as the protons.
-
-   CFG 8-17-06
-
-   Implemented synchrotron sampling, 22 Jan 07
-
-   fixed bugs in tetrad/compton scattering routines, 31 Jan 07
-
-   Implemented normalization for output, 6 Feb 07
-
-   Separated out different synchrotron sampling routines
-   into separate files, 8 Mar 07
-
-   fixed bug in energy recording; bug used Kcon[0] rather than 
-   Kcov[0] as energy, 18 Mar 07
-
-   major reorganization to encapsulate problem-dependent parts 5-6 Nov 07
-
  */
 
 #include "decs.h"
