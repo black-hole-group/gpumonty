986bcaaf301495cccb3c076fb9c6754133f47e1f
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-04-05 15:19:40 -0300

    mpi pseudocode

--- docs/mpi.py
+++ docs/mpi.py
@@ -1,15 +1,13 @@
-
-
 def mpimonty():
 	"""
 	Pseudocode for grmonty supporting MPI.
 	"""
 
-	# initialize random number generator in parallel
-	init_monty_rand(139 * myid + time(NULL));	/* Arbitrarily picked initial seed */
-
 	# reads data from GRMHD simulation
 	init_model(argv);
+
+	# sends HARM data to all nodes
+	mpi_send_harm_data();
 
 	# initializes global number of photons produced, scattered and recorded
 	# in the SED, will be updated after all slaves are done 
@@ -17,6 +15,11 @@ def mpimonty():
 	N_superph_recorded = 0;
 	N_scatt = 0;
 	quit_flag = 0;
+
+	# EXECUTED ON MULTI-CORE NODES ==================
+
+	# initialize random number generator in parallel
+	init_monty_rand(139 * myid + time(NULL));	/* Arbitrarily picked initial seed */
 
 	# parallel while loop that generates light rays
 	while (1):
@@ -32,6 +35,11 @@ def mpimonty():
 		track_super_photon(&ph);
 
 		N_superph_made += 1;
+
+	# END NODE PART =======================
+
+	# gather SEDs generated on all nodes
+	mpi_gather_sed();
 
 	# Once done with all photons, assemble SED
 	omp_reduce_spect();
