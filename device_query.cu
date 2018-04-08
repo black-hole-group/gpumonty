40f41c86c8a6c81f3a6b7d5e17e239aa3843cb97
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-04-08 01:34:56 -0300

    first version of devicequery

--- device_query.cu
+++ device_query.cu
@@ -1,8 +1,38 @@
-query GPU devices
-query GPU total and free memory
+/*
+Queries GPU for the amount of free memory.
+Returns max number of photons that GPU can hold at once.
 
-count size of HARM arrays
-get number of photons specified by user
+in: size of HARM arrays along each dimension
+out: max number of superphotons GPU can hold at once
 
-get max number of photons GPU can hold
-nmax = (free-sizeHARM-50MB)/100
+The GPU must hold the following n1xn2xn3 arrays in its 
+global memory during processing: 3*B, rho, T, 4*v
+*/
+#include <stdio.h> 
+
+int get_max_photons(int n1, int n2, int n3) {
+    cudaDeviceProp prop;
+    size_t free, total;
+    
+    // assumes one GPU
+    cudaGetDeviceProperties(&prop, 0); 
+    cudaMemGetInfo(&free, &total);
+
+    float memtotal=(float)prop.totalGlobalMem; // bytes
+    float memfree=(float)free; // bytes
+
+    // total size of HARM arrays in bytes, 9+1 arrays
+    // adds one extra array to leave room and avoid filling up the memory
+    sizeHARM=10.0f*n1*n2*n3;
+
+    // estimates max number of photons GPU can process at once
+    // based on size of of_photon struct
+    int nmax=(int)(memfree-sizeHARM)/(25.*4.);
+
+    printf("GPU model: %s\n", prop.name);
+    printf("Total memory = %f (GB)\n", memtotal/1e9);
+    printf("Free memory = %f (GB)\n", memfree/1e9);
+    printf("Max number of superphotons in each pass is %d\n", nmax);    
+
+    return nmax;
+}
