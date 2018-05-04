f6d44845c18e9096b416cfdba910a1e478e3f160
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-05-04 14:13:42 -0300

    ported compton.c functions to device

--- compton.cuh
+++ compton.cuh
@@ -1,46 +1,6 @@
 
-/***********************************************************************************
-    Copyright 2013 Joshua C. Dolence, Charles F. Gammie, Monika Mo\'scibrodzka,
-                   and Po Kin Leung
-
-                        GRMONTY  version 1.0   (released February 1, 2013)
-
-    This file is part of GRMONTY.  GRMONTY v1.0 is a program that calculates the
-    emergent spectrum from a model using a Monte Carlo technique.
-
-    This version of GRMONTY is configured to use input files from the HARM code
-    available on the same site.   It assumes that the source is a plasma near a
-    black hole described by Kerr-Schild coordinates that radiates via thermal 
-    synchrotron and inverse compton scattering.
-    
-    You are morally obligated to cite the following paper in any
-    scientific literature that results from use of any part of GRMONTY:
-
-    Dolence, J.C., Gammie, C.F., Mo\'scibrodzka, M., \& Leung, P.-K. 2009,
-        Astrophysical Journal Supplement, 184, 387
-
-    Further, we strongly encourage you to obtain the latest version of 
-    GRMONTY directly from our distribution website:
-    http://rainman.astro.illinois.edu/codelib/
-
-    GRMONTY is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    GRMONTY is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with GRMONTY; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-
-***********************************************************************************/
-
-#include "decs.h"
-#pragma omp threadprivate(r)
+//#include "decs.h"
+//#pragma omp threadprivate(r)
 
 /*
 
@@ -55,14 +15,10 @@ Canfield, Howard, and Liang, 1987, ApJ 323, 565.
 
 */
 
-void init_monty_rand(int seed)
-{
-	r = gsl_rng_alloc(gsl_rng_mt19937);	/* use Mersenne twister */
-	gsl_rng_set(r, seed);
-}
 
 /* return pseudo-random value between 0 and 1 */
-double monty_rand()
+__device__
+double d_monty_rand()
 {
 	return (gsl_rng_uniform(r));
 }
@@ -74,7 +30,7 @@ double monty_rand()
    find new wavevector $kp$ 
    
 */
-
+__device__
 void sample_scattered_photon(double k[4], double p[4], double kp[4])
 {
 	double ke[4], kpe[4];
@@ -173,7 +129,7 @@ Result goes out in vp.
 Assumes all four-velocities are given in orthonormal coordinates.
 
 */
-
+__device__
 void boost(double v[4], double u[4], double vp[4])
 {
 	double g, V, n1, n2, n3, gm1;
@@ -204,7 +160,7 @@ void boost(double v[4], double u[4], double vp[4])
    differential cross section */
 
 /* uses simple rejection scheme */
-
+__device__
 double sample_thomson()
 {
 	double x1, x2;
@@ -226,7 +182,7 @@ sample Klein-Nishina differential cross section.
 This routine is inefficient; it needs improvement.
 
 */
-
+__device__
 double sample_klein_nishina(double k0)
 {
 	double k0pmin, k0pmax, k0p_tent, x1;
@@ -260,7 +216,7 @@ double sample_klein_nishina(double k0)
    in units of m_e.  Unnormalized!
    
 */
-
+__device__
 double klein_nishina(double a, double ap)
 {
 	double ch, kn;
@@ -277,7 +233,7 @@ double klein_nishina(double a, double ap)
 	scattered.
 
 */
-
+__device__
 void sample_electron_distr_p(double k[4], double p[4], double Thetae)
 {
 	double beta_e, mu, phi, cphi, sphi, gamma_e, sigma_KN;
@@ -399,7 +355,7 @@ void sample_electron_distr_p(double k[4], double p[4], double Thetae)
    checked. 
    
 */
-
+__device__
 void sample_beta_distr(double Thetae, double *gamma_e, double *beta_e)
 {
 	double y;
@@ -423,7 +379,7 @@ void sample_beta_distr(double Thetae, double *gamma_e, double *beta_e)
    p. 572 et seq. 
    
 */
-
+__device__
 double sample_y_distr(double Thetae)
 {
 
@@ -470,6 +426,7 @@ double sample_y_distr(double Thetae)
 	return (y);
 }
 
+__device__
 double sample_mu_distr(double beta_e)
 {
 	double mu, x1, det;
