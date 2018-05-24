01f59c16cb540dc9ddffd9f1841a222b42a9430a
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-05-24 13:40:49 -0300

    starting to implement modifications from illinois branch here
    removed unnecessary constants

--- constants.h
+++ constants.h
@@ -47,46 +47,46 @@
 #define CL				(2.99792458e10		)	/* speed of light */
 #define ME				(9.1093826e-28		)	/* electron mass */
 #define MP				(1.67262171e-24		)	/* proton mass */
-#define MN				(1.67492728e-24		)	/* neutron mass */
-#define AMU				(1.66053886e-24		)	/* atomic mass unit */
+// #define MN				(1.67492728e-24		)	/* neutron mass */
+// #define AMU				(1.66053886e-24		)	/* atomic mass unit */
 #define HPL				(6.6260693e-27		)	/* Planck constant */
 #define HBAR			(HPL/(2.*M_PI)		)	/* Planck's consant / 2pi */
 #define KBOL			(1.3806505e-16		)	/* Boltzmann constant */
 #define GNEWT			(6.6742e-8			)	/* Gravitational constant */
 #define SIG				(5.670400e-5		)	/* Stefan-Boltzmann constant */
-#define RGAS			(8.3143e7			)	/* erg K^-1 mole^-1: ideal gas const */
-#define EV				(1.60217653e-12		)	/* electron volt in erg */
+// #define RGAS			(8.3143e7			)	/* erg K^-1 mole^-1: ideal gas const */
+// #define EV				(1.60217653e-12		)	/* electron volt in erg */
 #define SIGMA_THOMSON	(0.665245873e-24	)	/* Thomson cross section in cm^2 */
 #define JY				(1.e-23				)	/* Jansky (flux/freq. unit) in cgs */
 
 #define PC				(3.085678e18		)	/* parsec */
-#define AU				(1.49597870691e13	)	/* Astronomical Unit */
+// #define AU				(1.49597870691e13	)	/* Astronomical Unit */
 
 #define YEAR			(31536000.			)	/* No. of seconds in year */
-#define DAY				(86400.				)	/* No. of seconds in day  */
-#define HOUR			(3600.				)	/* No. of seconds in hour */
+// #define DAY				(86400.				)	/* No. of seconds in day  */
+// #define HOUR			(3600.				)	/* No. of seconds in hour */
 
 #define MSUN			(1.989e33			)	/* solar mass */
-#define RSUN			(6.96e10			)	/* Radius of Sun */
+// #define RSUN			(6.96e10			)	/* Radius of Sun */
 #define LSUN			(3.827e33				)	/* Luminousity of Sun */
-#define TSUN			(5.78e3				)	/* Temperature of Sun's photosphere */
+// #define TSUN			(5.78e3				)	/* Temperature of Sun's photosphere */
 
-#define MEARTH			(5.976e27			)	/* Earth's mass */
-#define REARTH			(6.378e8			)	/* Earth's radius */
+// #define MEARTH			(5.976e27			)	/* Earth's mass */
+// #define REARTH			(6.378e8			)	/* Earth's radius */
 
-#define DSGRA			(8.4e3 * PC			)	/* Distance from Earth to Sgr A*  */
+// #define DSGRA			(8.4e3 * PC			)	/* Distance from Earth to Sgr A*  */
 
-#define TCBR			(2.726				)	/* CBR temperature, from COBE */
+// #define TCBR			(2.726				)	/* CBR temperature, from COBE */
 
 /* 
    abundances, from M & B, p. 99
 */
 
-#define SOLX			(0.70				)	/* H */
-#define SOLY			(0.28				)	/* He */
-#define SOLZ			(0.02				)	/* Metals */
+// #define SOLX			(0.70				)	/* H */
+// #define SOLY			(0.28				)	/* He */
+// #define SOLZ			(0.02				)	/* Metals */
 
-#define FOUR_PI	(1.2732395447351626862)	/* 4/pi */
-#define FOUR_PISQ (0.40528473456935108578)	/* 4/pi^2 */
-#define THREEPI_TWO (4.7123889803846898577)	/* 3pi/2 */
+// #define FOUR_PI	(1.2732395447351626862)	/* 4/pi */
+// #define FOUR_PISQ (0.40528473456935108578)	/* 4/pi^2 */
+// #define THREEPI_TWO (4.7123889803846898577)	/* 3pi/2 */
 
