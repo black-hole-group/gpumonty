c0a79c1f7ed7f4782dbabb50f3540fda32a6df87
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-05-25 19:27:52 -0300

    moved common host-device dependencies to common header file, instead of repeating things

--- decs.h
+++ decs.h
@@ -16,114 +16,6 @@
 #include <omp.h>
 #include "constants.h"
 
-/* 
-  Basic definitions
-  ===================
-*/
-#define NDIM	4
-#define NPRIM	8
-
-/* number of variables necessary for each photon.
-   change this for 3D GRMHD simulations
-*/
-#define NPHVARS 25 
-
-/* Range of initial superphoton frequencies */
-#define NUMIN 1.e9
-#define NUMAX 1.e16
-
-#define THETAE_MAX	1000.	/* Only used for harm3d models */
-#define THETAE_MIN	0.3
-#define TP_OVER_TE	(3.)
-
-#define WEIGHT_MIN	(1.e31)
-
-#define RMAX 100.
-
-/* mnemonics for primitive vars; conserved vars */
-#define KRHO     0
-#define UU      1
-#define U1      2
-#define U2      3
-#define U3      4
-#define B1      5
-#define B2      6
-#define B3      7
-
-/* numerical convenience */
-#define SMALL	1.e-40
-
-/* physical parameters */
-#define MMW	0.5		/* mean molecular weight, in units of mp */
-
-/* mnemonics for dimensional indices */
-#define TT      0
-#define RR      1
-#define TH      2
-#define PH      3
-
-/** data structures *
-   We keep the struct below in order to reuse many functions
-   for photon generation
-*/
-struct of_photon { 
-	double X[NDIM];
-	double K[NDIM];
-	double dKdlam[NDIM];
-	double w;
-	double E;
-	double L;
-	double X1i;
-	double X2i;
-	double tau_abs;
-	double tau_scatt;
-	double ne0;
-	double thetae0;
-	double b0;
-	double E0;
-	double E0s;
-	int nscatt;
-};
-
-/* 
-Explanation of new format for of_photon:
-I am now defining a row-major 1D array that stores all photons
-produced that will be propagated in the GPU.
-
-It has the following structure:
-	ph2d[nphotons][NPHVARS] => ph1d[]
-	ph2d[i][j] => ph1d[i*NPHVARS+j] 
-where the first dimension corresponds to the different 
-superphotons and the second gives one of the variables from
-the old struct.
-
-Below are the mnemonics for the different photon variables
-*/
-#define X0      0
-#define X1      1
-#define X2      2
-#define X3      3
-#define K0_      4
-#define K1_      5
-#define K2_      6
-#define K3_      7
-#define D0      8
-#define D1      9
-#define D2      10
-#define D3      11
-#define W       12
-#define E_       13
-#define L_       14
-#define X1I     15
-#define X2I     16
-#define TAUA    17
-#define TAUS    18
-#define NE0     19
-#define TH0     20
-#define B0      21
-#define E0_      22
-#define E0S     23
-#define NS      24 // this guy was an int before, now it will be a double or float
 
 
 
@@ -133,25 +25,6 @@ Below are the mnemonics for the different photon variables
    ===============
 */
 
-
-struct of_photon { 
-	double X[NDIM];
-	double K[NDIM];
-	double dKdlam[NDIM];
-	double w;
-	double E;
-	double L;
-	double X1i;
-	double X2i;
-	double tau_abs;
-	double tau_scatt;
-	double ne0;
-	double thetae0;
-	double b0;
-	double E0;
-	double E0s;
-	int nscatt;
-};
 
 struct of_geom {
 	double gcon[NDIM][NDIM];
@@ -184,39 +57,6 @@ struct of_grid {
 	double th, phi;
 	int nlist;
 	int *in;
-};
-
-/*
-   structs required for passing variables to device
-*/
-
-
-// Data structure carrying all GRMHD variables
-struct simvars { 
-	// harm dimensions
-	int N1, N2, N3; 
-	int n_within_horizon;
-
-	/* some coordinate parameters */
-	double a;
-	double R0, Rin, Rh, Rout, Rms;
-	double hslope;
-	double startx[NDIM], stopx[NDIM], dx[NDIM];
-	double dlE, lE0;
-	double gam;
-	double dMsim;
-};
-
-
-struct allunits {
-	double M_unit;
-	double L_unit;
-	double T_unit;
-	double RHO_unit;
-	double U_unit;
-	double B_unit;
-	double Ne_unit;
-	double Thetae_unit;
 };
 
 
@@ -260,20 +100,11 @@ extern double Thetae_unit;
 
 extern double max_tau_scatt, Ladv, dMact; //bias_norm;
 
-/* some useful macros */
-#define DLOOP  for(k=0;k<NDIM;k++)for(l=0;l<NDIM;l++)
-#define INDEX(i,j,k)	(NPRIM*( (k) + N3*((j) + N2*(i))))
-#define MYSIN(x,sx) 	{							\
-			double _xp = (x)-M_PI; 					\
-			double _yp = _xp*(FOUR_PI - FOUR_PISQ*fabs(_xp)); 	\
-			sx = -_yp*(0.225*fabs(_yp)+0.775);			\
-			}
-#define MYCOS(x,cx) 	{							\
-			double _xp = (x)-THREEPI_TWO; 					\
-			_xp += (_xp<-M_PI)*2.*M_PI; 				\
-			double _yp = _xp*(FOUR_PI - FOUR_PISQ*fabs(_xp));		\
-			cx = _yp*(0.225*fabs(_yp)+0.775);			\
-			}
+
+/*
+  Subroutines and functions
+  ==========================
+*/
 
 /** model-independent subroutines **/
 /* core monte carlo/radiative transport routines */
