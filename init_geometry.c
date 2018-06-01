d3ee4fb5ff8c4b46f54cf8990c50cc939ff9f86d
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-06-01 12:19:20 -0300

    Removed omp private pragma from init_geometry.c. Code from this file were stricted executed by one thread so the pragma was unecessary.

--- init_geometry.c
+++ init_geometry.c
@@ -2,27 +2,26 @@
 
 #include "decs.h"
 
-/* 
+/*
 
    set up the metric (indicies both up and down), and the
    connection coefficients on a grid, based
-   on a HARM simulation grid.  
-   
+   on a HARM simulation grid.
+
    In principle the geometry could be sampled on a finer or
-   coarser grid than the simulation grid. 
+   coarser grid than the simulation grid.
 
    These routines are taken directly out of HARM.
 
-   They require the code be compiled against the 
+   They require the code be compiled against the
    Gnu scientific library (GSL).
 
    CFG 21 July 06
-   
+
 */
 
 gsl_matrix *gsl_gcov, *gsl_gcon;
 gsl_permutation *perm;
-#pragma omp threadprivate (gsl_gcov, gsl_gcon, perm)
 
 /* assumes gcov has been set first; returns determinant */
 double gdet_func(double gcov[][NDIM])
@@ -51,13 +50,13 @@ double gdet_func(double gcov[][NDIM])
 void gcon_func(double gcov[][NDIM], double gcon[][NDIM])
 {
 	int k, l, signum;
-	
+
 	if (gsl_gcov  == NULL) {
 		gsl_gcov = gsl_matrix_alloc(NDIM, NDIM);
 		gsl_gcon = gsl_matrix_alloc(NDIM, NDIM);
 		perm = gsl_permutation_alloc(NDIM);
 	}
-	
+
 
 	DLOOP gsl_matrix_set(gsl_gcov, k, l, gcov[k][l]);
 
