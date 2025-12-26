f8e2a9c8b548eac7b2ef3cbbfd2ae466028cbec8
Pedro Naethe <pedronaethemotta@id.uff.br>
2025-12-26 13:38:35 -0300

    Adding documentation page

--- src/memory.cu
+++ src/memory.cu
@@ -1,43 +1,7 @@
-/*
-Summary of the file: This file contain memory related functions that are used in the GPU code.
-*/
 #include "decs.h"
 #include "kernels.h"
 #include "memory.h"
 
-__host__ void create1DTextureObj(cudaTextureObject_t * texObj, double * ptr, cudaArray_t * cudaArray){
-	int width = N_ESAMP + 1;
-	float float_table[width];
-	for (int i = 0; i < width; i++) {
-		float_table[i] = (float)ptr[i];
-	}
-
-
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
-    CHECK_CUDA_ERROR(cudaMallocArray(cudaArray, &channelDesc, width, 1));
-
-	// Copy data from host to device
-    size_t spitch = width * sizeof(float);
-    CHECK_CUDA_ERROR(cudaMemcpy2DToArray(*cudaArray, 0, 0, float_table, spitch, spitch, 1, cudaMemcpyHostToDevice));
-	
-	// Specify texture object parameters
-    struct cudaResourceDesc resDesc;
-    memset(&resDesc, 0, sizeof(resDesc));
-    resDesc.resType = cudaResourceTypeArray;
-    resDesc.res.array.array = *cudaArray;
-	 // Specify texture description parameters
-	 struct cudaTextureDesc texDesc;
-	 memset(&texDesc, 0, sizeof(texDesc));
-	 texDesc.addressMode[0] = cudaAddressModeClamp;
-	 texDesc.filterMode = cudaFilterModeLinear;
-	 texDesc.readMode = cudaReadModeElementType;
-	 texDesc.normalizedCoords = 0;  // Using non-normalized coordinates
- 
-	 // Create texture object
-	 CHECK_CUDA_ERROR(cudaCreateTextureObject(texObj, &resDesc, &texDesc, NULL));
-	 // Destroy texture object and free CUDA array
-	 //CHECK_CUDA_ERROR(cudaFreeArray(cuArray));
-}
 
 __host__ void createdPTextureObj(cudaTextureObject_t * texObj, double * dP, cudaArray_t * cuArray) {	
 	const int nw = N1; //r
@@ -57,8 +21,6 @@ __host__ void createdPTextureObj(cudaTextureObject_t * texObj, double * dP, cuda
 	copyParams.extent   = make_cudaExtent(nx * ny, nz, nw);
 	copyParams.kind     = cudaMemcpyHostToDevice;	
 	CHECK_CUDA_ERROR(cudaMemcpy3D(&copyParams));
-	//cudaMemcpy3D(&copyParams);
-	//Array creation End
 
 	cudaResourceDesc    texRes;
 	memset(&texRes, 0, sizeof(texRes));
@@ -68,75 +30,16 @@ __host__ void createdPTextureObj(cudaTextureObject_t * texObj, double * dP, cuda
 	memset(&texDescr, 0, sizeof(texDescr));
 	texDescr.normalizedCoords = false ;
 	texDescr.filterMode = cudaFilterModePoint;
-	texDescr.addressMode[0] = cudaAddressModeClamp;   // clamp
+	texDescr.addressMode[0] = cudaAddressModeClamp;
 	texDescr.addressMode[1] = cudaAddressModeClamp;
 	texDescr.addressMode[2] = cudaAddressModeClamp;
 	texDescr.readMode = cudaReadModeElementType;
 	CHECK_CUDA_ERROR(cudaCreateTextureObject(texObj, &texRes, &texDescr, NULL));
 	return;
 }
-__host__ void createTableTextureObj(cudaTextureObject_t * texObj, double table[][NT + 1], const int width, const int height, cudaArray_t * cuArray) {	
-	float float_table[height][width];
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			float_table[i][j] = (float)table[i][j];
-		}
-	}
-
-
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
-    CHECK_CUDA_ERROR(cudaMallocArray(cuArray, &channelDesc, width, height));
-
-	// Copy data from host to device
-    size_t spitch = width * sizeof(float);
-    CHECK_CUDA_ERROR(cudaMemcpy2DToArray(*cuArray, 0, 0, float_table, spitch, 
-                                         width * sizeof(float), height, 
-                                         cudaMemcpyHostToDevice));
-	
-	// Specify texture object parameters
-    struct cudaResourceDesc resDesc;
-    memset(&resDesc, 0, sizeof(resDesc));
-    resDesc.resType = cudaResourceTypeArray;
-    resDesc.res.array.array = *cuArray;
-	 // Specify texture description parameters
-	 struct cudaTextureDesc texDesc;
-	 memset(&texDesc, 0, sizeof(texDesc));
-	 texDesc.addressMode[0] = cudaAddressModeClamp;
-	 texDesc.addressMode[1] = cudaAddressModeClamp;
-	 texDesc.filterMode = cudaFilterModeLinear;
-	 texDesc.readMode = cudaReadModeElementType;
-	 texDesc.normalizedCoords = 0;  // Using non-normalized coordinates
- 
-	 CHECK_CUDA_ERROR(cudaCreateTextureObject(texObj, &resDesc, &texDesc, NULL));
-}
 
 
 __host__ void transferParams() {
-	/*
-	This function will transfer the global variables to the GPU.
-
-	Variables:
-    @Ns - number of superphotons parameter
-    @N1 - number of zones in the r direction
-    @N2 - number of zones in the ph direction
-    @N3 - number of zones in the time direction
-    @dx - size of the zones
-    @hslope - slope of the theta to x2 conversion
-    @startx - starting x value
-    @stopx - stopping x value
-    @a - black hole spin
-    @Thetae_unit - unit of the dimensionless electron temperature
-    @wgt - array of weights for the superphoton distribution
-    @F - array of F values for the superphoton distribution
-    @nint - array of nint values for the superphoton distribution
-    @dndlnu_max - array of dndlnu_max values for the superphoton distribution
-    @K2 - array of second kind modified bessel function K2 values for the superphoton distribution
-    @bias_norm - normalization factor for the bias function
-    @max_tau_scatt - maximum scattering optical depth
-    @Rh - radius of theevent horizon
-
-	*/
-
 	int Ns_int = (int) params.Ns;
     cudaMemcpyToSymbol(d_Ns, &Ns_int, sizeof(int));
     cudaMemcpyToSymbol(d_dx, &dx, NDIM * sizeof(double));
@@ -165,31 +68,9 @@ __host__ void transferParams() {
 	cudaMemcpyToSymbol(d_beta_crit, &(params.beta_crit), sizeof(double));
 	cudaMemcpyToSymbol(d_thetae_max, &(params.Thetae_max), sizeof(double));
 	cudaMemcpyToSymbol(d_tp_over_te, &(params.tp_over_te), sizeof(double));
-
-
-
-	//cudaDeviceSetLimit(cudaLimitStackSize, 8000);
 }
 
 __host__ int setMaxBlocks(){
-	/*
-	This function will output the specs of the GPU in use. It will return the maximum number of blocks that can be used in the GPU.
-
-	Variables:
-	@device_id - the id of the GPU in use in case of multiple GPU systems
-	@maxThreadsPerBlock - the maximum number of threads per block
-	@maxBlocksPerMultiprocessor - the maximum number of blocks per multiprocessor
-	@numSMs - the number of SMs in the GPU
-	@max_block_number - the maximum number of blocks that can be used in the GPU
-
-	Observations:
-	This calculation depends on the GPU.
-	The first thing you wish to do is maximize the number of threads per block in a way that you can fit as many blocks as possible in the SMs.
-	For example, in case you have a GPU that fits 1536 threads per SM, 1024 threads per block is not a good idea because you end up underutilizing the SM (since you will only fit one block).
-	You should aim for 512 threads per block, so you can fit 3 blocks in the SM and utilize the maximum number of threads in the SM.
-
-	Deciding how many blocks to fit the GPU is an art and you should test it out yourself. The more blocks you fit, the more you can parallelize the code, but you may end up with a lot of overhead.
-	*/
 	int device_id;
     cudaGetDevice(&device_id);  
 	int maxThreadsPerBlock;
@@ -220,18 +101,6 @@ __host__ int setMaxBlocks(){
 
 __host__ void cummulativePhotonsPerZone(unsigned long long * generated_photons_arr, unsigned long long * d_index_to_ijk)
 {
-	/*
-	Create a cummulative sum array to be used in the sampling of photons
-
-	Parameters:
-	@generated_photons_arr - array containing the number of photons to be generated in each zone
-	@d_index_to_ijk - array containing the cummulative sum of the number of photons to be generated in each zone
-
-	Variables:
-	@nph - number of photons to be generated in each
-
-	*/
-
 	unsigned long long *h_index_to_ijk = (unsigned long long *)malloc(N1 * N2 * N3 * sizeof(unsigned long long));
 	unsigned long long *h_generated_photon_arr = (unsigned long long *)malloc(N1 * N2 * N3 * sizeof(unsigned long long));
 
@@ -247,22 +116,10 @@ __host__ void cummulativePhotonsPerZone(unsigned long long * generated_photons_a
 
 }
 
+
+
 __host__ unsigned long long photonsPerBatch(unsigned long long tot_nph, int * batch_divisions)
 {
-	/*
-	Estimate the number of photons to be generated per batch
-
-	Paremeters:
-	@tot_nph - total number of photons to be generated (in all partitions)
-
-	Variables:
-	@free_mem - free memory on the GPU
-	@required_mem - memory required to store the photons in the GPU
-	@superph_per_batch - number of photons to be generated per batch
-	@batch_divisions - number of batches to divide the photons into
-
-	*/
-
 	size_t free_mem, total_mem;
 	cudaError_t err = cudaMemGetInfo(&free_mem, &total_mem);
 	if (err != cudaSuccess) {
@@ -288,6 +145,9 @@ __host__ unsigned long long photonsPerBatch(unsigned long long tot_nph, int * ba
 	printf("\033[1;34mRequired partitions: %d\033[0m. Number of photons per partition: %d\n", *batch_divisions, (int)(tot_nph/(*batch_divisions)));
 	return (unsigned long long)(tot_nph/(*batch_divisions));
 }
+
+
+
 
 __host__ void allocatePhotonData(struct of_photonSOA *ph, unsigned long long size) {
     gpuErrchk(cudaMalloc(&(ph->X0), size * sizeof(double)));
--- src/memory.h
+++ src/memory.h
@@ -4,13 +4,93 @@ Declaration of the memory.cu functions
 
 #ifndef MEMORY_H
 #define MEMORY_H
+
+
+/**
+ * @brief Synchronizes global host simulation parameters with device memory symbols.
+ *
+ * Copies physics constants, grid dimensions, lookup tables and model-specific parameters from the 
+ * host to the device using `cudaMemcpyToSymbol`.
+ *
+ * @return void
+ */
 __host__ void transferParams();
+
+/**
+ * @brief Configures the optimal number of blocks to be used by CUDA kernels and generates GPU diagnostics.
+ *
+ * It multiplies the maximum number of blocks per multiprocessor by the total number of streaming multiprocessors (SMs) to 
+ * determine the total maximum number of blocks that can be launched concurrently on the GPU.
+ *
+ * @return The calculated maximum number of blocks for kernel launches.
+ *
+ */
 __host__ int setMaxBlocks();
+
+/**
+ * @brief Computes a cumulative sum of superphoton counts for zone-based photon sampling.
+ *
+ * Generates a cumulative sum array where the value at zone i equals the total number 
+ * of superphotons to be generated up to and including zone i-1.
+ *
+ * @param generated_photons_arr Array containing the number of photons to generate in each zone.
+ * @param d_index_to_ijk Array storing the cumulative sum of photons per zone.
+ * @return void
+ */
 __host__ void cummulativePhotonsPerZone(unsigned long long * generated_photons_arr, unsigned long long * d_index_to_ijk);
+
+/**
+ * @brief Calculates the batch size for GPU photon processing.
+ *
+ * Evaluates the available GPU RAM memory to determine the optimal number of 
+ * partitions. This prevents memory overflows by scaling the batch size 
+ * against the footprint of the `of_photon` structure.
+ *
+ * @param tot_nph Total number of photons to be processed in the simulation.
+ * @param batch_divisions Calculated number of batches.
+ * @return The number of photons assigned to each individual GPU batch.
+ */
 __host__ unsigned long long photonsPerBatch(unsigned long long tot_nph, int * batch_divisions);
+
+/**
+ * @brief Allocates device memory for photon data using a Structure of Arrays (SoA) layout.
+ *
+ * Initializes a `of_photonSOA` structure by allocating separate memory 
+ * buffers on the GPU for every photon property for both original and scattered photons at each batch. 
+ * * Using a Structure of Arrays (SoA) instead of an Array of Structures (AoS) is a 
+ * critical optimization that enables **coalesced memory access** patterns, 
+ * significantly increasing throughput for CUDA kernels.
+ * 
+ *
+ * @param ph Pointer to the `of_photonSOA` structure to be initialized.
+ * @param size The number of photon slots to allocate in each array.
+ *
+ */
 __host__ void allocatePhotonData(struct of_photonSOA *ph, unsigned long long size);
+
+
+/**
+ * @brief Deallocates device memory for a `of_photonSOA` structure.
+ *
+ * This function releases all individual GPU memory buffers that were previously allocated by `allocatePhotonData`. 
+ *
+ * @param ph Pointer to the `of_photonSOA` structure whose device members are to be freed.
+ *
+ */
 __host__ void freePhotonData(struct of_photonSOA * ph);
-__host__ void createTableTextureObj(cudaTextureObject_t * texObj, double table[][NT + 1], const int width, const int height, cudaArray_t * cuArray);
+
+
+/**
+ * @brief Creates a 3D texture object from a 4D data grid for the plasma primitive properties.
+ *
+ * Converts input `double` data to `float`, uploads it to a 3D CUDA array, and 
+ * initializes a texture object with point filtering and clamp addressing.
+ *
+ * @param texObj Pointer to the resulting texture object that stores the plasma primitive properties.
+ * @param dP  Input plasma properties.
+ * @param cuArray Pointer to the allocated 3D CUDA array resource.
+ *
+ * @note Maps 4D data into a 3D extent by combining the `nx` and `ny` dimensions.
+ */
 __host__ void createdPTextureObj(cudaTextureObject_t * texObj, double * dP, cudaArray_t * cuArray);
-__host__ void create1DTextureObj(cudaTextureObject_t * texObj, double * ptr, cudaArray_t * cudaArray);
 #endif
