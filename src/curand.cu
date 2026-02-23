12f1518e863755cfe724dc4d3cb0cbeb4cdb3646
pedro <pedronaethemotta@id.uff.br>
2026-02-23 13:30:15 -0600

    Added License to front page and updated docs to showcase this rep

--- src/curand.cu
+++ src/curand.cu
@@ -1,5 +1,22 @@
 #include "decs.h"
 #include "curand.h"
+/*
+ * GPUmonty - curand.cu
+ * Copyright (C) 2026 Pedro Naethe Motta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+ */
 
 __device__ void init_monty_rand(int seed) {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
