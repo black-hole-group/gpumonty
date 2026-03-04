ed288493c28d40c513e105b8166e3d03ec35f169
Abhishek Joshi <avjoshi2@illinois.edu>
2026-03-04 01:44:54 -0600

    updated config.h to include changes from main

--- src/config.h
+++ src/config.h
@@ -1,3 +1,20 @@
+/*
+ * GPUmonty - config.h
+ * Copyright (C) 2026 Pedro Naethe Motta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0.html>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,7 +39,7 @@
  * This defines the grid size and should ideally be a multiple of the number of Streaming Multiprocessors (SMs) on your GPU to maximize occupancy.
  * This is set through automatic GPU tuning in the Makefile unless ``BLOCK_TUNING`` is set to 0 in the Makefile.
  */
-#define N_BLOCKS 3456
+#define N_BLOCKS 256
 
 /**
  * The number of threads per block. 
